7936dceef8b3b2e35b89cf931e7f4807737647fc
Terje Brådland <terje.bradland@gmail.com>
2020-02-05 22:05:13 +0000

    Added Get Customers

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using static EF.Context;
+
+namespace EF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController
+    {
+        private readonly Context _context;
+
+        public CustomersController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IList<Customer> GetCustomers()
+        {
+            return _context.Set<Customer>().ToList();
+        }
+    }
+}
